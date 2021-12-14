23363bc180ed2c0ba5ed39610795bfcfd656d475
17elarg <74807487+17elarg@users.noreply.github.com>
2021-12-14 15:59:33 +0900

    컨트롤러, 페이지 추가
    아린 컨트롤러, 페이지 추가

---
+++ SistFinalProject/src/main/java/data/controller/ContentsController.java
@@ -0,0 +1,5 @@
+package data.controller;
+
+public class ContentsController {
+
+}
---
+++ SistFinalProject/src/main/java/data/controller/LoginController.java
@@ -0,0 +1,15 @@
+package data.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/login/main")
+	public String loginform() {
+
+		return "/login/loginmain";
+	}
+}
