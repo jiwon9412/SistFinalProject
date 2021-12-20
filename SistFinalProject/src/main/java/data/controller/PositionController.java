94a99fab64b28f88f72a61b66c9620eb72701690
StealSwan <vatican0@naver.com>
2021-12-20 16:22:48 +0900

    positionmain style 수정
    positionmain style 수정

--- SistFinalProject/src/main/java/data/controller/PositionController.java
+++ SistFinalProject/src/main/java/data/controller/PositionController.java
@@ -3,8 +3,6 @@ package data.controller;
 import java.util.List;
 
 import javax.servlet.http.HttpSession;
-
-import org.apache.jasper.tagplugins.jstl.core.Redirect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
