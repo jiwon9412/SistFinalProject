a1ba4175d4f1cfa0ce4f6c4056e88792f23119e8
ziwon <ziwon@LAPTOP-05IKSK6H>
2022-01-10 16:46:53 +0900

    11

--- SistFinalProject/src/main/java/data/controller/NoticesController.java
+++ SistFinalProject/src/main/java/data/controller/NoticesController.java
@@ -47,7 +47,7 @@ public class NoticesController {
 		int startPage;   //각 블럭의 시작페이지
 		int endPage;   //각 블럭의 끝페이지
 		int start;   //각페이지의 시작번호
-		int perPage=16;  //한페이지에 보여질 글수
+		int perPage=12;  //한페이지에 보여질 글수
 		int perBlock=5;  //한페이지에 보여지는 페이지 개수
 		//int no;
 
